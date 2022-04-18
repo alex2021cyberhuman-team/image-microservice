1a36998a3efe70f616f9660827f420b697523273
Alex2021cyberhuman <alex2021cyberhuman@gmail.com>
2022-04-18 09:13:33 +0500

    fix: Dublicate of BusinessLogicRegistrationExtensions

--- Conduit.Images.BusinessLogic/Extensions/BusinessLogicRegistrationExtensions.cs
+++ Conduit.Images.BusinessLogic/Extensions/BusinessLogicRegistrationExtensions.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Conduit.Images.BusinessLogic.Images.AssignArticleImage;
 using Conduit.Images.BusinessLogic.Images.RemoveArticleImage;
 using Conduit.Images.BusinessLogic.Images.RemoveUnassignedImages;
